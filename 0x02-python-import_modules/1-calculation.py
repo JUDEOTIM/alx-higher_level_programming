096363688b43a32ab0176fb0a0cc780378fec206
JUDEOTIM <otimjudeoj@gmail.com>
2023-04-05 01:11:36 -0700

    a program that imports functions from the file calculator_1.py, does some Maths, and prints the result.

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Print the number of and list of arguments."""
-    import sys
+    """Print the sum, difference, product, quotient of 10 and 5."""
+    from calculator_1 import add, sub, mul, div
 
-    length = len(sys.argv) - 1
-    if length == 0:
-        print("0 arguments.")
-    elif length == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(length))
+    a = 10
+    b = 5
 
-    for i in range(length):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
